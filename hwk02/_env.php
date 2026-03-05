aa57e5cc7aa467cb5fb4d79f1144cd7467d3530d
chloe-peng <cp3282@drexel.edu>
2026-03-05 16:17:35 -0500

    Delete hwk02/_env.php

--- hwk02/_env.php
+++
@@ -1,8 +0,0 @@
-<?php
-    return [
-        'DB_SERVER' => 'localhost',       
-        'DB_USER' => 'cp3282',
-        'DB_PASS' => 'WZU8qQPgdwXPxmG0',
-        'DB_NAME' => 'cp3282_db',
-    ];
-?>
