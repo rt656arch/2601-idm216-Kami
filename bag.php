4a6f3eee0e58c7ca3ee21bc49c864bfa82692872
rt656arch <rt656@drexel.edu>
2026-03-17 19:27:10 -0400

    update nav for bag.php

--- bag.php
+++ bag.php
@@ -219,7 +219,7 @@ foreach ($entry['drinks'] ?? [] as $drink_id) {
       <img src="images/navigation-bar-menu.png" alt="Menu">
       <span>Menu</span>
     </a>
-    <a href="home.html">
+    <a href="home.php">
       <img src="images/navigation-bar-home.png" alt="Home">
       <span>Home</span>
     </a>
